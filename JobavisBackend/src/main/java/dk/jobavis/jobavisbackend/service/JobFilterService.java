13e1fcdc65ae40fdc5bc6fff00a1d70f2482c5cd
Julius <axiiss@live.dk>
2025-03-15 19:59:10 +0100

    try-catch, logging, class and comment for future filtration implementation

---
+++ JobavisBackend/src/main/java/dk/jobavis/jobavisbackend/service/JobFilterService.java
@@ -0,0 +1,11 @@
+package dk.jobavis.jobavisbackend.service;
+
+import dk.jobavis.jobavisbackend.entity.JobSearchResult;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JobFilterService {
+
+
+
+}
